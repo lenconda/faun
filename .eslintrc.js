c8673750272dde16a14cc6b8ebe9804eb15948e2
lenconda <i@lenconda.top>
2020-05-04 01:46:36 +0800

    main functions and modules

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    'alloy',
+    'alloy/typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    },
+  },
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/typedef': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/no-empty-interface': 'off',
+    'eol-last': 2,
+    quotes: [2, 'single'],
+    'complexity': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+  },
+};
---
+++ src/flow.js
@@ -0,0 +1,28 @@
+function mountAssets(assets, parent) {
+  if (!Array.isArray(assets) || !(parent instanceof HTMLElement)) {
+    return;
+  }
+
+  assets.forEach(asset => {
+    if (asset instanceof HTMLElement) {
+      parent.appendChild(asset);
+    }
+  });
+}
+
+function removeAssets(assets) {
+  if (!Array.isArray(assets) || !(parent instanceof HTMLElement)) {
+    return;
+  }
+
+  assets.forEach(asset => {
+    if (asset instanceof HTMLElement) {
+      asset.remove();
+    }
+  });
+}
+
+export {
+  mountAssets,
+  removeAssets,
+}
