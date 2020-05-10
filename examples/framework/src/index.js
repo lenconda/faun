7df79c563e14c6b1c809de5cc176f6e08e4da3cb
lenconda <i@lenconda.top>
2020-05-10 11:51:44 +0800

    feat: add example cases

--- examples/framework/src/index.js
+++ examples/framework/src/index.js
@@ -25,8 +25,35 @@ app.registerModules({
   },
 });
 
-app.hooks.loading = function() {
-  console.log(111);
+app.hooks.loading = function(pathname) {
+  console.log('loading', this);
+  console.log('pathname: ', pathname);
+};
+
+app.hooks.loaded = function(pathname, sandbox) {
+  console.log('loaded', this);
+  console.log('pathname: ', pathname);
+  console.log('sandbox: ', sandbox);
+};
+
+app.hooks.mounted = function(pathname, sandbox) {
+  console.log('mounted', this);
+  console.log('pathname: ', pathname);
+  console.log('sandbox: ', sandbox);
+};
+
+app.hooks.beforeUnmount = function(prev, next) {
+  console.log('beforeUnmount', this);
+  console.log('prev: ', prev);
+  console.log('next: ', next);
+  return true;
+};
+
+app.hooks.unmounted = function(prev, next, sandbox) {
+  console.log('unmounted', this);
+  console.log('prev: ', prev);
+  console.log('next: ', next);
+  console.log('sandbox: ', sandbox);
 };
 
 app.run();
