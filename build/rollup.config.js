eb682212ec0352500076664acba485c44fa3c1b6
lenconda <i@lenconda.top>
2020-05-23 01:36:27 +0800

    refactor: switch from webpack into rollup

---
+++ build/rollup.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+import resolve from 'rollup-plugin-node-resolve';
+const { babel } = require('@rollup/plugin-babel');
+const babelOptions = require('../.babelrc.js');
+const terser = require('rollup-plugin-terser');
+const commonjs = require('rollup-plugin-commonjs');
+const packageJson = require('../package.json');
+const globals = require('rollup-plugin-node-globals');
+
+function resolveFile(filePath) {
+  return path.join(__dirname, '../', filePath);
+};
+
+const babelOpts = {
+  exclude: 'node_modules/**',
+  babelHelpers: 'runtime',
+};
+
+function generateBanner() {
+  return `/* polyatomic@${packageJson.version} */`;
+}
+
+Object.assign(babelOpts, babelOptions);
+
+module.exports = [
+  {
+    input: resolveFile('src/polyatomic.js'),
+    output: {
+      file: resolveFile('dist/polyatomic.min.js'),
+      format: 'umd',
+      name: 'Polyatomic',
+      banner: generateBanner(),
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      globals(),
+      babel(babelOpts),
+      terser.terser(),
+    ],
+  },
+];
