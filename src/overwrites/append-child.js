aaf826c0caa69203f30ae11badfe4be11387033f
lenconda <i@lenconda.top>
2020-07-21 00:44:28 +0800

    feat(mapper): add assetURLMapper support

---
+++ src/overwrites/append-child.js
@@ -0,0 +1,27 @@
+/**
+ * @file append-child.js
+ * @author lenconda<i@lenconda.top>
+ */
+
+export default function() {
+  const appendChild = Element.prototype.appendChild;
+
+  return {
+    intercept(callback) {
+      Element.prototype.appendChild = function(element) {
+        const nodeName = element.nodeName.toLowerCase();
+        if (/^script$|^link$/.test(nodeName)) {
+          const newElement = callback && typeof callback === 'function' && callback(element);
+          if (newElement) {
+            return appendChild.call(this, newElement);
+          }
+        }
+        return appendChild.call(this, element);
+      };
+    },
+
+    stop() {
+      Element.prototype.appendChild = appendChild;
+    },
+  };
+}
