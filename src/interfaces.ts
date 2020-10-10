e84189fa831434aa68562b408abe913d0a69f261
Peng Hanlin <i@lenconda.top>
2020-10-10 03:30:30 +0000

    rm: remove unused code or comments

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -68,14 +68,6 @@ export interface IPlugin {
   [key: string]: any;
 }
 
-// interface IHooks {
-//   loading: (pathname: string) => any;
-//   loaded: (pathname: string) => any;
-//   mounted: (pathname: string, sandbox: Sandbox) => any;
-//   beforeUnmount: (prev: string, next: string) => boolean;
-//   unmounted: (prev: string, next: string, sandbox: Sandbox) => any;
-// }
-
 export interface IHooks {
   loading?: (pathname: string) => any;
   loaded?: (pathname: string) => any;
