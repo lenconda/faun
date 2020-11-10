5933c1c72ce8700571c9268170ccd1c96df389e7
lenconda <i@lenconda.top>
2020-11-10 15:57:06 +0800

    chore(test): add test for faun and fetch

--- src/__tests__/fetch.test.js
+++ src/__tests__/fetch.test.js
@@ -3,4 +3,16 @@
  * @author lenconda<i@lenconda.top>
  */
 
-test('`fetch`');
+import fetch from '../fetch';
+
+jest.setTimeout(60000);
+
+test('`fetch` should be a function', async () => {
+  expect(fetch).toBeInstanceOf(Function);
+});
+
+test('`fetch` should request a API and return the response', async () => {
+  const result = await fetch('https://api.github.com');
+  expect(result).not.toBe('');
+  expect(result).not.toBe(null);
+});
